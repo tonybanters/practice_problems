a654d9ebb60ce4d5cd78d91e58148d36ba2f46ba
tony banter <asanchez@dispersivegroup.com>
2019-10-23 17:54:29 -0700

    Initial commits, with first few problems.

--- arrays.py
+++ arrays.py
@@ -26,11 +26,38 @@ class Solution:
             elif (prices[i] - buy > profit): # if the buy was higher, compare sell - buy with profit.
                 profit = prices[i] - buy
         return profit
+    
+    def containsDuplicate(self, nums: [int]) -> bool:
+        """This method will return true if the list contains 1 element at least twice,
+        and return false if every element is unique."""
+        # ... really simple, remove dupes and see if lists are equal length.
+        """ temp = list(set(nums))
+            return len(temp) != len(nums) """
+        # but this will not be O(n) in worst case, it will be every case O(n)
+        # can make small improvement, for average case, by iterating the set while making it
+        # and breaking the loop if set contains dupe
+        temp = set()
+        for number in nums:
+            if number in set:
+                return True
+            set.add(number)
+        return False
+        # for some reason this is slower, maybe for smaller lists. not sure, need to look into it
 
 
-
-# print(Solution().maxProfit([7,1,5,3,6,4]))
-# print(Solution().maxProfit([7,6,4,3,1]))
+    def productExceptSelf(self, nums: [int]) -> [int]:
+        """This method will take a list of ints as input, and return a list of
+        products of the list, excluding the ith index, without using division, in O(n) time"""
+        result = []
+        left = [1]*len(nums)
+        right = [1]*len(nums)
+        for i in range(1,len(nums)):
+            left[i] = left[i-1] * nums[i-1]
+        for i in reversed(range(len(nums)-1)):
+            right[i] = right[i+1] * nums[i+1]
+        for i in range(len(nums)):
+            result.append(left[i]*right[i])
+        return result
 
 
 
